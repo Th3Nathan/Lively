a6b7b4b7ac1be8ca0e1d7e26a8f95070eae8c4d8
Nathan Vass <nathanevass@gmail.com>
2017-11-25 17:25:46 -0500

    animation delay on tooltip

--- src/Tooltip.tsx
+++ src/Tooltip.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import './Tooltip.css';
 import Hoverable from './Hoverable';
 import { HoverState } from './interfaces';
+
 interface Props {
     data: {
         primary: String;
@@ -16,20 +17,32 @@ class Tooltip extends React.Component<Props, {}> {
     }
 
     render() {
+        let {primary, secondary, orientation} = this.props.data;
+        //padd secondary with a space if it exists
+        if (secondary) {
+            secondary = " " + secondary; 
+        }
         return (
             <Hoverable>
-                {(state: HoverState) => {
-                    let hovering = state.hovering;
-                    let tooltip;
-                    if (hovering) {
-                        tooltip = <div>I AM A TOOLTIP{this.props.data.primary}</div>;
-                    } else {
-                        tooltip = <div>there is no toolbip showing now yo{this.props.data.secondary}</div>;
-                    }
+                {({hovering}: HoverState) => {
+                    let style = hovering ? {'visibility': 'visible'} : {'visibility': 'hidden'};
+                    let tooltip = hovering ? (
+                        <div style={style} className="Tooltip delay">
+                            <i className="fa fa-caret-up Tooltip-caret" aria-hidden="true"></i>
+                            <div className="Tooltip-box">
+                                <div className={`Tooltip-primary-${orientation}`}>
+                                    {primary}
+                                    <span className="Tooltip-secondary">
+                                        {secondary}
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    ) : null
                     return (
-                        <div className="Tooltip">
+                        <div className="">
                             {this.props.children}
-                            {/* {tooltip}   */}
+                            {tooltip}  
                         </div>
                     );
                 }}
