0f67f33ae558acb3a4f332f518fb3e50ca421652
Nathan Vass <nathanevass@gmail.com>
2017-11-26 00:12:25 -0500

    modal header item

--- src/NotificationsModal.tsx
+++ src/NotificationsModal.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import ReactModal from 'react-modal';
+import ModalSectionHeader from './ModalSectionHeader';
 
 export interface Props {
     isOpen: boolean;
@@ -22,15 +23,32 @@ class NotificationsModal extends React.Component<Props, State> {
         'right': '0px',
         'bottom': '0px',
         'position': 'absolute',
-        'z-index': 998,
-        'background': 'blue'
+        'z-index': '998',
+        'background': 'rgba(0, 0, 0, 0)',
+        'cursor': 'default',
+
     };
+
+    modalStyle = {
+        'background': 'white',
+        'top': '39px',
+        'left': '172px',
+        'border-radius': '12px',
+        'width': '260px',
+        'position': 'absolute',
+        'height': '500px',
+        'padding': '17px 0px',
+        'box-sizing': 'border-box',
+        'box-shadow': '0 5px 10px rgba(0,0,0,.12)',
+        'border': '1px solid rgba(0,0,0,.15)',
+        'outline': 'none'
+    }
     render() {
         return (
         <ReactModal
-            isOpen={this.props.isOpen}
+            isOpen={true}
             onRequestClose={this.props.closeModal}
-            style={{ overlay: this.overlayStyle, content: {} }}
+            style={{ overlay: this.overlayStyle, content: this.modalStyle }}
             contentLabel="Example Modal"
             overlayClassName="ReactModal__Overlay"
             className="ReactModal__Content"
@@ -39,7 +57,15 @@ class NotificationsModal extends React.Component<Props, State> {
             shouldCloseOnOverlayClick={true}
             parentSelector={() => document.body}
         >
-                IM THE MODAL ITS MEEE
+            <ModalSectionHeader text={"Snooze notification:"}/>
+            <ul className="NotificationsModal-options">
+                <li>20 minutes</li>
+                <li>1 hour</li>
+                <li>2 hours</li>
+                <li>4 hours</li>
+                <li>8 hours</li>
+                <li>24 hours</li>
+            </ul>
         </ReactModal>
     );
     }
